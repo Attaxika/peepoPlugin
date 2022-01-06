da33e669d5696b16fbf850ff6cc91e7f3f0c5a0c
Attaxika <brandon.kimball2012@gmail.com>
2022-01-05 21:44:09 -0700

    Readability

--- src/main/java/com/peepo/peepoPlugin/CommandsCompleter.java
+++ src/main/java/com/peepo/peepoPlugin/CommandsCompleter.java
@@ -9,17 +9,22 @@ import java.util.List;
 
 public class CommandsCompleter implements TabCompleter {
     private static List<String> num = new ArrayList<>();
-    private static List<String> first = new ArrayList<>();
+    private static List<String> firstArg = new ArrayList<>();
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         num.add("<0-100>");
-        first.add("Accelerate");
-        first.add("SlimeRate");
-        first.add("disable");
+        firstArgSetter();
         if(args.length == 1) {
-            return first;
+            return firstArg;
         } else if(args.length == 2) {
             return num;
         } return null;
+    }
+
+    //Moved this down here for readability & future-usage
+    private void firstArgSetter() {
+        firstArg.add("Accelerate");
+        firstArg.add("SlimeRate");
+        firstArg.add("disable");
     }
 }
